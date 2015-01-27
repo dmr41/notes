ee6bd0f1e1c2fbb0591365d8414a6399c26cfb6e
David Rivers <dmr41.rivers@gmail.com>
2015-01-27 11:47:51 -0700

    added alert message for different conditions

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -5,24 +5,30 @@ export default Ember.ArrayController.extend({
     newNote: function() {
       var body = this.get('noteCopy');
       var title =this.get('noteCopy2');
-      if (body || title) {
+      if (body && title) {
         var note = this.store.createRecord('note', { body: body, title: title });
         this.set('noteCopy', '');
         this.set('noteCopy2', '');
         note.save();
-
+        this.flashMessage('', 'Noted!', 2000);
+      }
+      else if (body) {
+        this.flashMessage('', 'You forgot the title.');
+      }
+      else if (title) {
+        this.flashMessage('', "You left your note blank. What's up with that?");
+      }
+      else {
+        this.flashMessage('', 'Fill in both fields.');
       }
     },
+
     deleteNote: function (note_id) {
       var note = this.store.find('note', note_id).then(function(note) {
         note.deleteRecord();
         note.save();
       });
+      this.flashMessage('', 'Deleted');
     },
-
-    formSubmitted: function() {
-      this.flashMessage('success', 'Congratulations! Your changes have been saved');
-  }
-
   }
 });
