5960db5059aae2e8bbb26f73a948e0e5152b9468
David Rivers <dmr41.rivers@gmail.com>
2015-01-29 08:12:41 -0700

    heroku test 2

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,11 +2,10 @@ import DS from "ember-data";
 
 // export default DS.LSAdapter.extend({});
 //
-// export default DS.ActiveModelAdapter.extend({
-//   host: 'http://localhost:3000'});
+// export default DS.RESTAdapter.extend({
+//   // host: 'http://localhost:3000'
+//   host: 'https://dmr-notes-api.herokuapp.com'
+//   });
 
-
-export default DS.RESTAdapter.extend({
-  // host: 'http://localhost:3000'
-  host: 'https://dmr-notes-api.herokuapp.com/'
-  });
+export default DS.ActiveModelAdapter.extend({
+   host: 'https://dmr-notes-api.herokuapp.com'});
